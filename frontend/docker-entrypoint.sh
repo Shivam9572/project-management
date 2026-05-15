cdeddcd05445953fa32db3510b0e7e0c40c26447
Shivam9572 <shivamroy9572@gamil.com>
2026-05-16 00:18:41 +0530

    remove looop

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 set -e
 
-envsubst '${PORT}' < /etc/nginx/conf.d/default.conf > /tmp/default.conf
-mv /tmp/default.conf /etc/nginx/conf.d/default.conf
-
-exec "$@"
+nginx -g "daemon off;"
